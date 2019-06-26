7b264c738e6ecbfc7050f3493a8a794bd37bd6ea
James <nmfuntouchable@gmail.com>
2019-06-25 15:15:40 -1000

    created a test of a javascript file creator

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+function generateFile(filename, text) {
+    const element = document.createElement('a');
+
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+// Start file download.
+function download() {
+    // Generate download of hello.txt file with some content
+    const text = document.getElementById('text-container').innerHTML;
+    const filename = 'test.txt';
+
+    generateFile(filename, text);
+}
