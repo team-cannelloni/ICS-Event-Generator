489d5b78ac81c55760309a074e94749875df43d6
James <nmfuntouchable@gmail.com>
2019-07-03 09:24:15 -1000

    refactored validation

--- javascript/main.js
+++ javascript/main.js
@@ -43,11 +43,21 @@ function download() {
   }
 
   function validateRequiredDates(start, startId, end, endId) {
-
+    console.log(start + ' ' + end);
+    return false;
   }
 
   function validateRequiredTimes(start, startId, end, endId) {
 
+  }
+
+  function validate(data) {
+    return {
+      xWrCalname: validateRequiredTextInput(data.xWrCalname, 'event-name'),
+      description: validateRequiredTextInput(data.description, 'event-description'),
+      summary: validateRequiredTextInput(data.summary, 'event-summary'),
+      location: validateRequiredTextInput(data.location, 'event-location')
+    };
   }
 
   /** Shows a warning for a field that was not properly filled out
@@ -63,70 +73,56 @@ function download() {
     warningElement.style.opacity = 1;
   }
 
-  const eventName = document.getElementById('event-name').value;
-  const eventDescription = document.getElementById('event-description').value;
-  const eventSummary = document.getElementById('event-summary').value;
-  const eventLocation = document.getElementById('event-location').value;
-
-  if (validateRequiredTextInput(eventName, 'event-name') && 
-  validateRequiredTextInput(eventDescription, 'event-description') &&
-  validateRequiredTextInput(eventSummary, 'event-summary') &&
-  validateRequiredTextInput(eventLocation, 'event-location') && 
-  validateRequiredDates(eventStartDate, 'event-start-date', eventEndDate, 'event-end-date') &&
-  validateRequiredTimes(eventStartTime, 'event-start-time', eventEndTime, 'event-end-time')
-  ) {
-    console.log('all good');
-  } else {
-    return false;
-  }
-  const eventClassification = document.getElementById('event-classification').value;
-  const eventLatitude = document.getElementById('event-latitude').value;
-  const eventLongitude = document.getElementById('event-longitude').value;
   const eventStartTime = document.getElementById('event-start-time').value;
   const eventEndTime = document.getElementById('event-end-time').value;
   const eventStartDate = document.getElementById('event-start-date').value;
   const eventEndDate = document.getElementById('event-end-date').value;
-  const eventRRule= document.getElementById('event-repeat-rule').value;
-  const eventPriority= document.getElementById('event-priority').value;
+  const eventLatitude = document.getElementById('event-latitude').value;
+  const eventLongitude = document.getElementById('event-longitude').value;
 
 
   const data = {
     begin: 'VCALENDAR',
     version: '2.0',
     prodid: '-//ical.marudot.com//iCal Event Maker',
-    xWrCalname: 'Test Event',
+    xWrCalname: document.getElementById('event-name').value,
     calscale: 'GREGORIAN',
     begintz: 'VTIMEZONE',
     tzid: 'Pacific/Honolulu',
     tzurl: 'http://tzurl.org/zoneinfo-outlook/Pacific/Honolulu',
     xLicLocation: 'Pacific/Honolulu',
-    beginzone:'Pacific/Honolulu',
+    beginzone: 'Pacific/Honolulu',
     tzoffsetfrom: '-1000',
     tzoffsetto: '-1000',
     tzname: 'HST',
     dtstart: '19700101T000000',
     dtend: '19700101T010000',
-    rrule: 'FREQ=DAILY;COUNT=10',
+    rrule: `FREQ=DAILY;COUNT=${document.getElementById('event-repeat-rule').value}`,
     endtype: 'STANDARD',
     endtz: 'VTIMEZONE',
     beginevent: 'VEVENT',
     dtstamp: '20190621T035133Z',
     uid: '20190621T035133Z - 2089128844@marudot.com',
-    priority: '0',
-    class: 'PUBLIC',
-    summary: 'Test Event',
+    priority: document.getElementById('event-priority').value,
+    class: document.getElementById('event-classification').value,
+    summary: document.getElementById('event-summary').value,
     geo: '21.29693;-157.81711',
-    description: 'This is a test of the event creator',
-    location: 'UH Manoa'
+    description: document.getElementById('event-description').value,
+    location: document.getElementById('event-location').value
   };
 
-  data.xWrCalname = eventName;
-  data.summary = eventSummary;
-  data.description = eventDescription;
-  data.location = eventLocation;
-  data.priority = eventPriority;
-  data.class = eventClassification;
-  data.rrule = 'FREQ=DAILY;COUNT=' + eventRRule;
+
+  const errors = validate(data);
+
+  const valid = Object.keys(errors).forEach(key => {
+    if (!errors[key]) {
+      return false;
+    }
+  });
+
+  if (!valid) {
+    return;
+  }
 
   // Generate download of hello.txt file with some content
   const dataArray = [`BEGIN:${data.begin}`,
@@ -138,7 +134,7 @@ function download() {
   `TZID:${data.tzid}`,
   `TZURL:${data.tzurl}`,
   `X-LIC-LOCATION:${data.xLicLocation}`,
-  `BEGIN:STANDARD`,
+    `BEGIN:STANDARD`,
   `TZOFFSETFROM:${data.tzoffsetfrom}`,
   `TZOFFSETTO:${data.tzoffsetto}`,
   `TZNAME:${data.tzname}`,
@@ -151,14 +147,14 @@ function download() {
   `CLASS:${data.class}`,
   `RRULE:${data.rrule}`,
   `UID:${data.uid}`,
-  'DTSTART;TZID=Pacific/Honolulu:20190621T120000',
-  'DTEND;TZID=Pacific/Honolulu:20190621T120000',
+    'DTSTART;TZID=Pacific/Honolulu:20190621T120000',
+    'DTEND;TZID=Pacific/Honolulu:20190621T120000',
   `SUMMARY:${data.summary}`,
   `GEO:${data.geo}`,
   `DESCRIPTION:${data.description}`,
   `LOCATION:${data.location}`,
-  'END:VEVENT',
-  'END:VCALENDAR'];
+    'END:VEVENT',
+    'END:VCALENDAR'];
 
   let text = '';
   dataArray.forEach(line => text += `${line}\r\n`);
